a74254dada7c96759f22f466e11f8a2b1395c3b6
Liam <mcmac.lm@gmail.com>
2023-06-22 18:53:13 -0400

    feat: base app structure for Overview components
    Added components and sub components into their respective folders for
    the overview component.
    
    Closes :
    https://trello.com/c/S8BvDhtg/40-overview-making-components-subcomponents-structure-3

---
+++ my-app/src/Components/1 - Overview/ProductInfo/SubComponents/Share.jsx
@@ -0,0 +1,14 @@
+import React, {useState, useEffect} from 'react';
+
+
+const Share = () => {
+
+
+  return (
+  <div>
+    <h1> ADDING SUBCOMPONENTS LATER</h1>
+  </div>
+  )
+};
+
+export default Share;
