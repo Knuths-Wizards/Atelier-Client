25e95cda01212be43a497d1761856b4d4462b0f2
Liam <mcmac.lm@gmail.com>
2023-06-26 09:20:52 -0400

    build: adding placeholder env feat: stars added

--- my-app/src/Components/1 - Overview/ProductInfo/SubComponents/StarRating.jsx
+++ my-app/src/Components/1 - Overview/ProductInfo/SubComponents/StarRating.jsx
@@ -16,13 +16,46 @@ const StarRating = ({ratings}) => {
     return (sum/count);
   }
   const ratingAvg = calcAvgRating(ratingObj);
-  console.log('RATING AVERAGE',ratingAvg)
+  const fillPercentage = (ratingAvg/5) * 100;
 
   return (
-  <div>
-    <h1> RATING AVERAGE: {ratingAvg}</h1>
-  </div>
-  )
+    <div className="flex items-center">
+      <div className="relative flex">
+        {[...Array(5)].map((_, index) => (
+          <svg
+            key={index}
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            className={`w-6 h-6 ${
+              index < Math.floor(ratingAvg) ? 'text-yellow-400' : 'text-gray-300'
+            }`}
+          >
+            <defs>
+              {index + 1 === Math.ceil(ratingAvg) && (
+                <linearGradient id={`starGrad${index}`}>
+                  <stop offset={`${fillPercentage}%`} stopColor="gold" />
+                  <stop offset={`${fillPercentage}%`} stopColor="lightgray" />
+                </linearGradient>
+              )}
+            </defs>
+            <path
+              fill={
+                index + 1 <= Math.floor(ratingAvg)
+                  ? 'gold'
+                  : index + 1 === Math.ceil(ratingAvg)
+                  ? `url(#starGrad${index})`
+                  : 'lightgray'
+              }
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z"
+              />
+          </svg>
+        ))}
+      </div>
+      <p className="ml-2">{ratingAvg.toFixed(2)}</p>
+    </div>
+  );
 };
 
 export default StarRating;
