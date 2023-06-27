35234067928a90ef1f2ce74e67ae9c6e05967548
Liam <mcmac.lm@gmail.com>
2023-06-27 15:06:17 -0400

    feat: completed ProductInfo Entirely, added Style Select
    ProductInfo price properly updates with the selected Style
    Style Select only needs to display a checkmark, currently renders a
    black border on selection.
    
    closes:
    https://trello.com/c/LYMDh6q2/49-overview-implement-product-info-section-5
    
    related:
    https://trello.com/c/DZJnFpfs/51-overview-develop-style-selector-5
    (cherry picked from commit 9ee2c6aa45f33ea426d36f82e5227fdc591950f3)

--- my-app/src/Components/1 - Overview/AddCart/AddCart.jsx
+++ my-app/src/Components/1 - Overview/AddCart/AddCart.jsx
@@ -1,14 +1,62 @@
 import React, {useState, useEffect} from 'react';
-
+import SizeSelector from './SubComponents/SizeSelector.jsx'
+import QuantitySelector from './SubComponents/QuantitySelector.jsx'
+import CartButton from './SubComponents/CartButton.jsx'
 
 const AddCart = () => {
+  const [size, setSize] = useState(null)
+  const [quantity, setQuantity] = useState(null);
 
+  const handleSize = (size) => {
+    setSize(size)
+  }
+
+  //sizes available depend on style, quantity depends on size selected
 
   return (
   <div>
-    <h1> ADDING SUBCOMPONENTS LATER</h1>
+    <SizeSelector size = {size} changeSize={handleSize}/>
+    <QuantitySelector></QuantitySelector>
+    <CartButton></CartButton>
   </div>
   )
 };
 
 export default AddCart;
+
+/*{ STYLE DATA BELOW
+  "style_id": 220998,
+  "name": "Forest Green & Black",
+  "original_price": "140.00",
+  "sale_price": null,
+  "default?": true,
+  "photos": [
+    ...
+  ],
+  "skus": {
+      "1281032": {
+          "quantity": 8,
+          "size": "XS"
+      },
+      "1281033": {
+          "quantity": 16,
+          "size": "S"
+      },
+      "1281034": {
+          "quantity": 17,
+          "size": "M"
+      },
+      "1281035": {
+          "quantity": 10,
+          "size": "L"
+      },
+      "1281036": {
+          "quantity": 15,
+          "size": "XL"
+      },
+      "1281037": {
+          "quantity": 4,
+          "size": "XL"
+      }
+  }
+},*/
--- my-app/src/Components/1 - Overview/ProductInfo/ProductInfo.jsx
+++ my-app/src/Components/1 - Overview/ProductInfo/ProductInfo.jsx
@@ -1,35 +1,42 @@
 import React, {useState, useEffect} from 'react';
 import StarRating from './SubComponents/StarRating.jsx'
 import Share from './SubComponents/Share.jsx'
-import {fetchProductData, fetchProductReviewMetaData, fetchProductReviews} from '../ovRoutes.js'
-const ProductInfo = ({product, changeProduct, productID}) => {
+import { fetchProductReviewMetaData, fetchProductReviews} from '../ovRoutes.js'
+const ProductInfo = ({product, changeProduct, productID, style, price}) => {
   const [ratings,setRatings] = useState({});
   const [reviewsCount,setReviewsCount] = useState({});
-  const [price, setPrice] = useState('')
 
   useEffect(() => {
     Promise.all([
       fetchProductReviewMetaData(productID),
       fetchProductReviews(productID),
-      fetchProductData(productID)
     ])
       .then(([reviewMetaData, reviews, productData]) => {
         setRatings(reviewMetaData);
         setReviewsCount(reviews.results.length);
-        setPrice(productData.default_price);
       })
       .catch((err) => {
         console.log('AXIOS error fetching reviews meta data', err);
       });
   }, []);
 
+  //check if sale price
+  const salePrice = style.sale_price;
   return (
   <div className = "product-info-container text-left">
     <h1> TESTING COMPONENTS </h1>
     {reviewsCount > 0 && <StarRating ratings = {ratings} reviewsCount={reviewsCount}></StarRating>}
     <div className="text-xs uppercase">{product.category}</div>
     <div className="text-2xl font-semibold">{product.name}</div>
-    <div className="text-xs mt-2 mb-2" style={{fontSize: '0.6em'}}>${price}</div>
+    <div className="text-xs mt-2 mb-2" style={{fontSize: '0.6em'}}>
+        {salePrice ? (
+          <>
+            <del className="text-gray-500">${style.original_price}</del> <span>On SALE! ${salePrice}</span>
+          </>
+        ) : (
+          <>${price}</>
+        )}
+    </div>
     <div className="text-xs pr-5">{product.description}</div>
     <Share></Share>
 
