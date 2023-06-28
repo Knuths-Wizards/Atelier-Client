145ce8986e9cb76a2b4d82b72cdcc40fc6e33270
Michael Johnson <michael.johnson7c5@gmail.com>
2023-06-28 12:10:45 -0400

    Implemented serverIO

--- my-app/src/Components/RatingsReviews/serverIO.js
+++ my-app/src/Components/RatingsReviews/serverIO.js
@@ -1,17 +1,36 @@
 import axios from '../../axiosAtelier'
-import path from 'path'
+import path from 'path-browserify'
 
-const api = process.env.REACT_APP_API_BASE_URL
+const API = process.env.REACT_APP_API_BASE_URL
 
 const serverIO = {
   getReviews: (productId) => {
-
-    return Promise.resolve([])
+    return axios({
+      url: path.join(API, 'reviews'),
+      method: 'GET',
+      params: {
+        product_id: productId
+      }
+    })
+    .then((response)=>{
+      return response.data.results
+    })
+    .catch((err)=>{
+      console.log('CTRL: Error getting reviews')
+      console.error(err.message)
+      return []
+    })
   },
 
   castVote: (reviewId) => {
-
-    return Promise.resolve(null)
+    console.log('PUT', path.join(API, 'reviews', reviewId.toString(), 'helpful'))
+    return axios({
+      url: path.join(API, 'reviews', reviewId.toString(), 'helpful'),
+      method: 'PUT'
+    })
+    .catch((err)=>{
+      console.log('CTRL: Error casting vote')
+    })
   },
 
   reportReview: (reviewId) => {
