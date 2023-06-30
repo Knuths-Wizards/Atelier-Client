c4c9f7bd21baa00fb56ef737cd57b6f4800e3694
Meade Reamy <meade@reamy.com>
2023-06-30 00:56:45 -0400

    Feature: added modal for click on RC Card img

--- my-app/src/Components/4 - RelatedComparison/Common/ImageSlides.jsx
+++ my-app/src/Components/4 - RelatedComparison/Common/ImageSlides.jsx
@@ -1,8 +1,6 @@
 import React, { useRef } from 'react';
-import { ScrollMenu, VisibilityContext } from 'react-horizontal-scrolling-menu';
+import { VisibilityContext } from 'react-horizontal-scrolling-menu';
 import 'react-horizontal-scrolling-menu/dist/styles.css';
-import { Stars } from '../Common/Stars.jsx';
-import { LeftArrow, RightArrow } from '../Common/ImageArrows.jsx';
 
 export default function ImageSlides({ smallImg, largeImg, itemId}) {
   const visibility = React.useContext(VisibilityContext);
@@ -10,7 +8,7 @@ export default function ImageSlides({ smallImg, largeImg, itemId}) {
 
 
   const showModal = () => {
-    if (modalRef.current) {
+    if (modalRef.current && largeImg !== null) {
       modalRef.current.showModal();
     }
   };
@@ -23,42 +21,15 @@ export default function ImageSlides({ smallImg, largeImg, itemId}) {
         height: "300px"
       }}
       >
-      <img width='300px' height='449px' style={{ marginTop: "0px", width: '200px', height: '300px' }} src={smallImg} alt='' />
+      <img width='300px' height='449px' style={{ marginTop: "0px", width: '200px', height: '300px' }} src={smallImg} alt='' onClick={showModal} />
+      <dialog ref={modalRef} className="modal">
+          <form method="dialog" className="modal-box">
+            <img src={largeImg} alt=''/>
+          </form>
+          <form method="dialog" className="modal-backdrop">
+            <button>close</button>
+          </form>
+        </dialog>
     </div>
-  );
-}
-
-
-
-function ModalContent({ogFeatures, features, title, ogTitle}) {
-  let features1 = features.map(item => item.feature);
-  let features2 = ogFeatures.map(item => item.feature);
-
-  let mergedFeatures = [...new Set([...features1, ...features2])];
-
-  return (
-    <table className='table table-zebra'>
-      <thead>
-        <tr>
-          <th>Feature</th>
-          <th>{title}</th>
-          <th>{ogTitle}</th>
-        </tr>
-      </thead>
-      <tbody>
-        {mergedFeatures.map(feature => {
-          let featuresValue = features.find(item => item.feature === feature)?.value || 'X';
-          let ogFeaturesValue = ogFeatures.find(item => item.feature === feature)?.value || 'X';
-
-          return (
-            <tr>
-              <td>{feature}</td>
-              <td>{featuresValue}</td>
-              <td>{ogFeaturesValue}</td>
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
   );
 }
