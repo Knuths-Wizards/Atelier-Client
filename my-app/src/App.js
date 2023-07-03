04ebbe0608eb9f8d391e11a445216a62d430d289
Meade Reamy <meade@reamy.com>
2023-07-03 19:54:18 -0400

    Fix: appjs now imports correctly

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,8 +2,8 @@ import React from 'react';
 import logo from './logo.svg';
 import './styles/App.css';
 import QA from './Components/QA/QA.jsx';
-import Overview from './Components/1 - Overview/Overview.jsx'
-import RelatedCompare from './Components/4 - RelatedComparison/RelatedComparison'
+import Overview from './Components/Overview/Overview.jsx'
+import RelatedCompare from './Components/RelatedComparison/RelatedComparison'
 import RatingsReviews from './Components/RatingsReviews/RatingsReviews'
 
 function App() {
