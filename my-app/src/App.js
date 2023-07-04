a1b0517111a946611c36f96f387b9f31f3caf909
Liam <mcmac.lm@gmail.com>
2023-07-04 17:58:44 -0400

    feat: searchbar build: productID state is now at the top level for App
    closes : https://trello.com/c/l5dboUKQ/88-overview-searchbar-3
    related:
    https://trello.com/c/GPX9xcWU/89-global-refactor-necessary-states-to-the-global-level-3

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './styles/App.css';
 import QA from './Components/QA/QA.jsx';
@@ -7,9 +7,12 @@ import RelatedCompare from './Components/RelatedComparison/RelatedComparison'
 import RatingsReviews from './Components/RatingsReviews/RatingsReviews'
 
 function App() {
+  const [productID, setProductID] = useState('37311');
+
   return (
     <div className="App">
       <div>
+<<<<<<< HEAD
         <Overview productID = {productID} setProductID={setProductID}></Overview>
       </div>
       <div className="centered">
@@ -20,6 +23,18 @@ function App() {
       </div>
       <div className="centered">
         <RatingsReviews productID = {productID}></RatingsReviews>
+=======
+      <Overview productID = {productID} setProductID={setProductID}></Overview>
+      </div>
+      <div>
+      <RelatedCompare ></RelatedCompare>
+      </div>
+      <div>
+        <QA productID = {productID}></QA>
+      </div>
+      <div>
+      <RatingsReviews productID = {productID}></RatingsReviews>
+>>>>>>> e49c173... feat: searchbar
       </div>
     </div>
   );
