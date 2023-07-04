052660ffbd38f04e44f76ffb59e06b8adf2e58b5
Michael Johnson <michael.johnson7c5@gmail.com>
2023-07-04 09:08:31 -0400

    fixing merge conflicts

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import logo from './logo.svg';
 import './styles/App.css';
-import Main from './Components/QA/Main.jsx';
-import Overview from './Components/1 - Overview/Overview.jsx'
-import RelatedCompare from './Components/4 - RelatedComparison/RelatedComparison'
+import QA from './Components/QA/QA.jsx';
+import Overview from './Components/Overview/Overview.jsx'
+import RelatedCompare from './Components/RelatedComparison/RelatedComparison'
 import RatingsReviews from './Components/RatingsReviews/RatingsReviews'
 
 function App() {
