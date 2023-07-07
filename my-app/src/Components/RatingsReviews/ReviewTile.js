defdbbf73c4757500c6299214d8b8e2e96661222
Michael Johnson <michael.johnson7c5@gmail.com>
2023-07-07 11:09:13 -0400

    review styling

--- my-app/src/Components/RatingsReviews/ReviewTile.js
+++ my-app/src/Components/RatingsReviews/ReviewTile.js
@@ -1,5 +1,6 @@
 import serverIO from './serverIO'
 import { format } from 'date-fns'
+import StarRating from './StarRating'
 
 const ReviewTile = (props) => {
   const { review, refresh } = props
@@ -19,14 +20,16 @@ const ReviewTile = (props) => {
   }
 
   return (
-    <div data-testid='review' className='card glass m-2'>
-      <div data-testid='stars'>{rating} Stars</div>
-      <div>{name} {format(new Date(date), 'MM/dd/yyyy')}</div>
-      <h3>{summary}</h3>
-      <p>{body}</p>
-      <div>Was this review helpful?
-        <button onClick={handleVote}>
-          {` Yes (${helpfulness})`}
+    <div data-testid='review' className='card glass m-2 z-1 overflow-clip'>
+      <p className='flex items-center p-2 bg-base-200 z-0'>
+        <span data-testid='stars'><StarRating id={`${id}-rating`} rating={rating} size={'md'}/></span>
+        <span className={'text-xl align-bottom'}>&nbsp;"{summary}"&nbsp;</span>
+        <span className={'italic align-bottom'}>- {name}, {format(new Date(date), 'MM/dd/yyyy')}</span>
+      </p>
+      <p className='p-2'>{body}</p>
+      <div className='p-2'>Was this review helpful?&nbsp;
+        <button className={'hover:text-accent'} onClick={handleVote}>
+          {`Yes (${helpfulness})`}
         </button>
       </div>
     </div>
