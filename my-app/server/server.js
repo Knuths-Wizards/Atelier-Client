cd41b16fb8e8c56329ae5341a2d8c25593c9c892
Kevin Glidden <gliddensolutions@gmail.com>
2023-07-12 18:34:51 -0500

    temp express server and db set up

---
+++ my-app/server/server.js
@@ -0,0 +1,23 @@
+//database connection
+import { sql } from './db.js';
+
+//environment variables
+require('dotenv').config();
+const port = process.env.PORT || 3000;
+
+
+//importing modules
+const path = require('path');
+const express = require('express');
+
+//initializing express app
+const app = express();
+
+let dir = path.join(__dirname, '..', 'public');
+app.use(express.static(dir));
+
+app.get('/', (req, res) => {
+  app.render('index.html');
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
