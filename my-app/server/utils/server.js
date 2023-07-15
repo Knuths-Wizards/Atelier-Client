66ad7bbe36363f474d37e30e56d9207608c59d99
Kevin Glidden <gliddensolutions@gmail.com>
2023-07-15 14:53:11 -0500

    added reviews routes

--- my-app/server/utils/server.js
+++ my-app/server/utils/server.js
@@ -13,28 +13,31 @@ function createServer() {
     res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
   });
 
+  // Products
   app.get("/products", controller.products.getProducts);
-
   app.get("/products/:product_id", controller.products.getProductInfo);
-
   app.get("/products/:product_id/styles", controller.products.getProductStyles);
-
   app.get("/products/:product_id/related", controller.products.getRelatedProducts);
 
-  app.get("/qa/questions", controller.qna.getQuestions);
+  // Reviews Routes
+  app.get('/reviews', controller.reviews.getReviews);
+  app.get('/reviews/meta', controller.reviews.getMeta);
+  app.put('/reviews/:review_id/helpful', controller.reviews.updateHelpful);
+  app.put('/reviews/:review_id/report', controller.reviews.updateReported);
 
+  app.get('/reviews/:product_id', controller.reviews.getReviews);
+  app.post('/reviews', controller.reviews.postReview);
+
+  // Questions Routes
+  app.get("/qa/questions", controller.qna.getQuestions);
   app.get("/qa/questions/:question_id/answers", controller.qna.getAnswers);
 
   app.post("/qa/questions", controller.qna.postQuestion);
-
   app.post("/qa/questions/:question_id/answers", controller.qna.postAnswer);
 
   app.put("/qa/questions/:question_id/helpful", controller.qna.markQuestionHelpful);
-
   app.put("/qa/questions/:question_id/report", controller.qna.reportQuestion);
-
   app.put("/qa/answers/:answer_id/helpful", controller.qna.markAnswerHelpful);
-
   app.put("/qa/answers/:answer_id/report", controller.qna.reportAnswer);
 
   return app;
