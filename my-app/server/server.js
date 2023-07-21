09b50fd50244278f29dbfa971d0f05294862bd65
Kevin Glidden <gliddensolutions@gmail.com>
2023-07-21 14:31:28 -0500

    corrected build dir path

--- my-app/server/server.js
+++ my-app/server/server.js
@@ -9,13 +9,13 @@ const port = process.env.PORT || 3000;
 const app = express();
 
 // init express dir
-let dir = path.join(__dirname, '..', '..', 'build');
+let dir = path.join(__dirname, '..', 'build');
 app.use(express.static(dir));
 app.use(express.json());
 
 // ----- ROUTES -----
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', '..', 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
 });
 
 // Products
